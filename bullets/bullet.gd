16c79f4b0ccab03c2e0945a54e50bee255541170
Stone-keep <radek12345@gmail.com>
2026-05-13 22:11:35 +0200

    Add player animations and initial level layout

--- bullets/bullet.gd
+++ bullets/bullet.gd
@@ -1,14 +1,18 @@
 extends Area2D
 
 var direction: Vector2
-var speed := 300
+var speed := 200
 
 func _physics_process(delta: float) -> void:
     position += direction * speed * delta
+    
+func _ready() -> void:
+    var crosshair_tween = get_tree().create_tween()
+    crosshair_tween.tween_property($BulletSprite, "scale", Vector2(1.0, 1.0), 0.2).from(Vector2.ZERO)
 
 func _on_screen_exited() -> void:
     queue_free()
 
 func setup(pos: Vector2, dir: Vector2):
-    position = pos + dir * 30
+    position = pos + dir * 15
     direction = dir
--- characters/player.gd
+++ characters/player.gd
@@ -1,30 +1,61 @@
 extends CharacterBody2D
 
-const SPEED := 200.0
+const SPEED := 100.0
 const FRICTION := SPEED / 0.1
-const JUMP_STRENGTH := 400.0
+const JUMP_STRENGTH := 300.0
 const GRAVITY_FACTOR := 1.0
 var direction: float
 var can_shoot := true
+
+var gun_directions = {
+	Vector2i(1, 0):   0,
+	Vector2i(1, 1):   1,
+	Vector2i(0, 1):   2,
+	Vector2i(-1, 1):  3,
+	Vector2i(-1, 0):  4,
+	Vector2i(-1, -1): 5,
+	Vector2i(0, -1):  6,
+	Vector2i(1, -1):  7
+}
 
 signal player_shot(pos: Vector2, dir: Vector2)
 
 
 func _physics_process(delta: float) -> void:
-	get_input(delta)
+	get_input()
 	handle_gravity(delta)
+	handle_movement(delta)
+	handle_torso_animation()
+	handle_crosshair_position()
 	move_and_slide()
 
-func get_input(delta: float) -> void:
+func get_input() -> void:
 	direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP_STRENGTH
 	if Input.is_action_just_pressed("shoot") and can_shoot:
 		try_shoot()
+
+func handle_movement(delta: float) -> void:
+	if is_on_floor():
+		if direction:
+			velocity.x = direction * SPEED
+			$PlayerLegs.flip_h = direction < 0
+			$PlayerAnimation.current_animation = "run"
+		else:
+			velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+			$PlayerAnimation.current_animation = "idle"
+	else:
+		if direction:
+			velocity.x = direction * SPEED
+			$PlayerLegs.flip_h = direction < 0
+
+		$PlayerAnimation.current_animation = "jump"
+
+func handle_torso_animation() -> void:
+	var mouse_position = get_local_mouse_position().normalized()
+	var rounded_position = Vector2i(round(mouse_position.x), round(mouse_position.y))
+	$PlayerTorso.frame = gun_directions[rounded_position]
 
 func handle_gravity(delta: float) -> void:
 	if not is_on_floor():
@@ -33,12 +64,19 @@ func handle_gravity(delta: float) -> void:
 func shoot():
 	player_shot.emit(position, get_local_mouse_position().normalized())
 	can_shoot = false
+	var crosshair_tween = get_tree().create_tween()
+	crosshair_tween.tween_property($Crosshair, "scale", Vector2(0.1, 0.1), 0.2)
+	crosshair_tween.tween_property($Crosshair, "scale", Vector2(0.5, 0.5), 0.2)
 	$ReloadTimer.start()
 
 func try_shoot() -> void:
 	if not can_shoot:
 		return
 	shoot()
+
+func handle_crosshair_position() -> void:
+	var mouse_position = get_local_mouse_position().normalized()
+	$Crosshair.position = mouse_position * 50
 
 func _on_reload_timer_timeout() -> void:
 	can_shoot = true
